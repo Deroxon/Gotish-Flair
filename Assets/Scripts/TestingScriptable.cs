95abedb5b9204e697339c167e2736988e9e1d6f8
Deroxon <55814631+Deroxon@users.noreply.github.com>
2026-06-08 22:31:05 +0200

    enemies commit

---
+++ Assets/Scripts/TestingScriptable.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "TestingScriptable", menuName = "Scriptable Objects/TestingScriptable")]
+public class TestingScriptable : ScriptableObject
+{
+    public string monsterName;
+
+
+
+}
